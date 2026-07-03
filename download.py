c33067f700d20201c1afc8a02d3f50c3dd87404f
mahan <mahan.48415@gmail.com>
2026-07-03 19:03:47 +0330

    init

--- download.py
+++ download.py
@@ -1,8 +1,5 @@
-import studies  # ثبت ساختار داده‌ها
 from neuralset.events import Study
 
-# اگر داده‌های MEG را می‌خواهید "Pinet2024Meg" و اگر EEG را می‌خواهید "Pinet2024Eeg" بگذرانید
 study = Study(name="Pinet2024Meg", path="SpanishBCBL")
-study.download()        # دانلود از هگینگ فیس
-events = study.build()  # ساخت ساختار رویدادها
-print("دانلود و آماده‌سازی با موفقیت انجام شد!")
+study.download()
+events = study.build()
