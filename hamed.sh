067e7da20f3c38351011cd8c42191d81b6aa101d
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2026-07-17 13:55:46 +0330

    +

--- hamed.sh
+++ hamed.sh
@@ -4,3 +4,12 @@ python start_trainer.py --batch_size 2 --epochs 1
 
 # git add .
 # git commit -m "Add my changes"
+# git push -u origin hamedrq7/hamed
+
+# on A5000 
+# git clone <repo-url>
+# cd <repo-folder>
+
+# git switch your-name/my-changes
+
+# git pull hamedrq7/hamed
