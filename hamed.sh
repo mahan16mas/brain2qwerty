b37c38a309e8e28f044c0735d18c95d55271c41e
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2026-07-17 13:39:01 +0330

    Add my changes

--- hamed.sh
+++ hamed.sh
@@ -1,7 +1,6 @@
 python start_trainer.py --batch_size 2 --epochs 1
 
-# git switch -c hamed/hamed
+# git switch -c hamedrq7/hamed
 
 # git add .
 # git commit -m "Add my changes"
-# git push -u hamed/hamed
