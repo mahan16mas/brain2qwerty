48e438d5af12d516dff2eedb3872459b9f0d9c91
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2026-07-21 02:42:23 +0330

    +

--- loaders.py
+++ loaders.py
@@ -9,7 +9,7 @@ from utils.data_loader import get_input
 from typing import Tuple, List
 import pickle
 import os
-CHUNK_SIZE = 4
+CHUNK_SIZE = 16
 
 def ctc_collate(batch: list[tuple[torch.Tensor, str, int]]):
     xs, ys, ds = zip(*batch)
