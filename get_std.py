42c76fcf09865b9d17f3f3da94b751e89d38b6eb
mahan <mahan.48415@gmail.com>
2026-07-24 16:01:28 +0330

    eval

--- get_std.py
+++ get_std.py
@@ -3,39 +3,35 @@ import pandas as pd
 import pickle
 import numpy as np
 
-datasets = ["nejm", "nlp10", "nlp21", "speech"]
-for dataset in datasets:
-    dataset_name = dataset
-    filename = f"{dataset_name}_pre_lm.csv"
+seeds = range(5)
+datasets = ["nlp21", "nlp10", "speech", "nejm", ]
+ds_names = ["NLP21", "NLP10", "SPEECH24", "SPEECH45"]
+borders = [99, 40, 400, 565]
+
+for seed in seeds:
+    rows = []
+    filename = f"meta_seed{seed}.csv"
+    for dataset, ds_name, border in zip(datasets, ds_names, borders):
+        is_nlp = "NLP" in ds_name
+        dataset_name = dataset
+        filename = f"{dataset_name}_seed{seed}.csv"
 
 
-    def get_mean(file_path):
-        err_dist = pickle.load(open(file_path, 'rb'))
-        mean = sum([s[0] for s in err_dist]) / sum([s[1] for s in err_dist])
-        return mean
+        def get_mean(err_dist):
+            mean = sum([s[0] for s in err_dist]) / sum([s[1] for s in err_dist])
+            return mean
 
-    name_to_folders = {
-        "META":[f"{dataset}-smallx"] + [f"{dataset_name}-smallx-{seed}" for seed in range(1, 5)]
-    }
-
-    data = []
-    for name, folders in name_to_folders.items():
-        scores = []
-        for folder in folders:
-            file = f"{folder}/{'' if 'Multi' not in name else dataset + '_'}evalStats"
-            scores.append(get_mean(file))
-        mean = np.mean(scores)
-        std = np.std(scores)
-        data.append({"model":name, "mean":mean, "std":std})
+        err_dist_path = f"{dataset}-smallx{f'-{seed}' if seed != 0 else ''}/evalStats"
+        err_dist = pickle.load(open(err_dist_path, "rb"))
+        seen_cer = get_mean(err_dist[:border])
+        unseen_cer = get_mean(err_dist[border:])
+        mean_cer = get_mean(err_dist)
+        rows.append({"experiment_setup":f"NoLM-{'C' if is_nlp else 'P'}ER", "dataset_name":dataset, "model_name":"META",
+                         "seen_test_day_average":seen_cer, "unseen_test_day_average":unseen_cer, "mean_test_day_average":mean_cer,})
 
 
 
-    df_new = pd.DataFrame(data)
 
-    if os.path.exists(filename):
-        df_old = pd.read_csv(filename)
-        df_final = pd.concat([df_old, df_new], ignore_index=True)
-    else:
-        df_final = df_new
+    df_new = pd.DataFrame(rows)
 
-    df_final.to_csv(filename, index=False)
+    df_new.to_csv(filename, index=False)
