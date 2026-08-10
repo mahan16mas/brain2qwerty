907c0aa467cae5c80f35e13150786016f7dfab36
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2026-08-10 21:55:19 +0330

    +

--- loaders.py
+++ loaders.py
@@ -265,7 +265,7 @@ def merge_by_borders(data1, borders1, data2, borders2):
 def get_dataset_loaders_nlp_21(
         dataset_name,
         batch_size,
-        gauss_in=True
+        gauss_in=True,
 ):
     train_input = get_input(
         os.path.join(dataset_name, "seed_model_training_data/mat/"),
@@ -301,8 +301,61 @@ def get_dataset_loaders_nlp_21(
     valid_input = merge_by_borders(valid_input_1, borders_1, valid_input_2, borders_2)
     assert len(valid_input) == len(valid_input_1) + len(valid_input_2)
     valid_input = valid_input_0 + valid_input
-    valid_set = HandwritingDataset(valid_input)
     train_set = HandwritingDataset(train_input)
+    valid_set = HandwritingDataset(valid_input)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True,
+                              num_workers=4, pin_memory=True, collate_fn=ctc_collate,
+                              persistent_workers=True)
+    test_loader = DataLoader(
+        valid_set,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=0,
+        pin_memory=True,
+        collate_fn=ctc_collate,
+    )
+    return train_loader, test_loader, None
+
+def get_dataset_loaders_nlp_21_with_noise(
+        dataset_name,
+        batch_size,
+):
+    train_input = get_input(
+        os.path.join(dataset_name, "seed_model_training_data/mat/"),
+        norm=True,
+        gauss=False,
+        train=True,
+        gauss_sigma=2.0
+    )
+    valid_input_0 = get_input(
+        os.path.join(dataset_name, "seed_model_training_data/mat/"),
+        norm=True,
+        gauss=False,
+        train=False,
+        valid=True,
+        gauss_sigma=2.0
+    )
+    valid_input_1, borders_1 = get_input(
+        os.path.join(dataset_name, "online_evaluation_data/no_recalibration/mat/"),
+        norm=True,
+        gauss=False,
+        train=False,
+        gauss_sigma=2.0,
+        return_borders=True
+    )
+    valid_input_2, borders_2 = get_input(
+        os.path.join(dataset_name, "online_evaluation_data/recalibration/mat/"),
+        norm=True,
+        gauss=False,
+        train=False,
+        gauss_sigma=2.0,
+        return_borders=True
+    )
+    valid_input = merge_by_borders(valid_input_1, borders_1, valid_input_2, borders_2)
+    assert len(valid_input) == len(valid_input_1) + len(valid_input_2)
+    valid_input = valid_input_0 + valid_input
+    train_set = HandwritingDataset(train_input)
+    valid_set = HandwritingDataset(valid_input)
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True,
                               num_workers=4, pin_memory=True, collate_fn=ctc_collate,
                               persistent_workers=True)
@@ -324,7 +377,7 @@ def get_dataset_loaders(
         gauss_in=True,
         speech=True,
         nlp_10=False,
-        is_nejm=False
+        is_nejm=False,
     ):
     if speech:
         if is_nejm: return get_dataset_loaders_speech_nejm(dataset_name, batch_size, gauss_in)
@@ -332,4 +385,20 @@ def get_dataset_loaders(
     if not nlp_10:
         return get_dataset_loaders_nlp_21(dataset_name, batch_size, gauss_in)
     return get_dataset_loaders_nlp_10(dataset_name, batch_size, gauss_in)
+
+
+
+def get_dataset_loaders_with_noise(
+        dataset_name,
+        batch_size,
+        gauss_in=True,
+        speech=True,
+        nlp_10=False,
+        is_nejm=False,
+    ):
+    if speech:
+        return None
+    if not nlp_10:
+        return get_dataset_loaders_nlp_21_with_noise(dataset_name, batch_size, gauss_in)
+    return None
 
