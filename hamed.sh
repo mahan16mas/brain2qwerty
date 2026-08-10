6f5c7a8dd8383b3f93983a5cecfd998eacd1917b
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2026-08-10 08:58:13 +0330

    +

--- hamed.sh
+++ hamed.sh
@@ -5,7 +5,7 @@
 
 # CUDA_VISIBLE_DEVICES=0 python start_trainer.py --cebra_rnn --out_dir "nlp21_meta_CEBRARNN(h-1024_o-1024_reflect_rnn-dim-1024_rnn-layers-5_bidir)-bs8-mahanLR" --dataset_path /mnt/data/hossein/Hossein_workspace/nips_cetra/mahan/CORP/CORP_data_release --batch_size 8 --epochs 50 --conv_dropout 0.5 --dropout_input 0.2 --do_wandb --cebra_model_name Offset5Model --cebra_hidden_dim 1024 --cebra_out_dim 1024 --cebra_pad_mode replicate --rnn_hidden 1024 --rnn_layers 5 --bidir
 
-CUDA_VISIBLE_DEVICES=0 python start_trainer.py --out_dir nlp21_meta_default_50_w_noise_ --dataset_path /mnt/data/hossein/Hossein_workspace/nips_cetra/mahan/CORP/CORP_data_release --batch_size 16 --epochs 50 --conv_dropout 0.5 --dropout_input 0.2 --do_wandb
+CUDA_VISIBLE_DEVICES=0 python start_trainer.py --out_dir "nlp21_meta_default_50_w_noise(0.8-0.2)" --dataset_path /mnt/data/hossein/Hossein_workspace/nips_cetra/mahan/CORP/CORP_data_release --batch_size 16 --epochs 50 --conv_dropout 0.5 --dropout_input 0.2 --do_wandb --add_noise --whiteNoiseSD 0.8 --constantOffsetSD 0.2 
 
 ### GOOD ARGS
 # python start_trainer.py --cebra_patch_encoder --out_dir 'nlp21_meta_CEBRATRANSFORMER_defaultCEBRA(h-1024_o-1024_reflect_tr-d-4_tr-h-2)_bs16' --dataset_path "/data/hossein/mm_project/CORP_data_release" --batch_size 16 --epochs 50 --conv_dropout 0.5 --dropout_input 0.2 --do_wandb --cebra_model_name "Offset5Model" --cebra_hidden_dim 1024 --cebra_out_dim 1024  --cebra_pad_mode 'replicate' --transformer_depth 4 --transformer_head 2
