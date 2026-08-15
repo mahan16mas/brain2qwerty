626f4f5afb8661c856fdf3d82169ea4eb97c5766
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2026-08-15 14:22:09 +0330

    +

--- meta_model_for_cebra_pipeline.py
+++ meta_model_for_cebra_pipeline.py
@@ -98,7 +98,7 @@ class MetaModel(nn.Module):
 
         cnn_out, lengths = self._cnn_forward(neuro, lengths, None, None) # you might wanna pass lengths if you do padding later 
 
-        return self._transformer_forward(cnn_out, lengths), _, _
+        return self._transformer_forward(cnn_out, lengths), None, None 
 
 
 if __name__=="__main__":
