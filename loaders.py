5a52addcf55ce55f92f0d19e15788b57b5169817
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2026-08-23 19:53:34 +0330

    _

--- loaders.py
+++ loaders.py
@@ -446,7 +446,7 @@ def get_dataset_loaders_with_noise(
 if __name__=="__main__": 
     import os 
     dataset_name = "/mnt/data/hossein/Hossein_workspace/nips_cetra/mm_project/CORP/CORP_data_release"
-    tr, te = get_dataset_loaders_nlp_21(dataset_name, 2, True, chunk_size=4,
+    tr, te, _ = get_dataset_loaders_nlp_21(dataset_name, 2, True, chunk_size=4,
                 stride=4,)
     from tqdm import tqdm 
     for batch in tqdm(tr):
