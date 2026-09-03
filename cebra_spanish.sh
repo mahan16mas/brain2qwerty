fbe18d5b629f421ee9e3a33c6496f91c2e5b837d
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2026-09-04 01:18:25 +0330

    +

--- cebra_spanish.sh
+++ cebra_spanish.sh
@@ -9,12 +9,19 @@
 
 
 ### Adding noise? 
-python -m brain2qwerty_v1.start_cebra --out_dir SpanishBCBL-small-cebra-default-CebUnfold_True-wNoise_0.8_0.2 --bidir --cebra_unfolder --gru --offset 4 --batchSize 64  --random_offset --hidden 1024 --dropout 0.4 --layers 5 --nBatch 10000 --kernel 25 --stride 4 --seed 5 --do_wandb --gradClipValue 10.0 --whiteNoiseSD 0.8 --constantOffsetSD 0.2 
+# python -m brain2qwerty_v1.start_cebra --out_dir SpanishBCBL-small-cebra-default-CebUnfold_True-wNoise_0.8_0.2 --bidir --cebra_unfolder --gru --offset 4 --batchSize 64  --random_offset --hidden 1024 --dropout 0.4 --layers 5 --nBatch 10000 --kernel 25 --stride 4 --seed 5 --do_wandb --gradClipValue 10.0 --whiteNoiseSD 0.8 --constantOffsetSD 0.2 
 
-python -m brain2qwerty_v1.start_cebra --out_dir SpanishBCBL-small-cebra-default-CebUnfold_True-wNoise_0.8_0.2-GaussIn --bidir --cebra_unfolder --gru --offset 4 --batchSize 64  --random_offset --hidden 1024 --dropout 0.4 --layers 5 --nBatch 10000 --kernel 25 --stride 4 --seed 5 --do_wandb --gradClipValue 10.0 --whiteNoiseSD 0.8 --constantOffsetSD 0.2 --gauss_in
+# python -m brain2qwerty_v1.start_cebra --out_dir SpanishBCBL-small-cebra-default-CebUnfold_True-wNoise_0.8_0.2-GaussIn --bidir --cebra_unfolder --gru --offset 4 --batchSize 64  --random_offset --hidden 1024 --dropout 0.4 --layers 5 --nBatch 10000 --kernel 25 --stride 4 --seed 5 --do_wandb --gradClipValue 10.0 --whiteNoiseSD 0.8 --constantOffsetSD 0.2 --gauss_in
 
-python -m brain2qwerty_v1.start_cebra --out_dir SpanishBCBL-small-cebra-default-CebUnfold_True-wNoise_1.2_0.6 --bidir --cebra_unfolder --gru --offset 4 --batchSize 64  --random_offset --hidden 1024 --dropout 0.4 --layers 5 --nBatch 10000 --kernel 25 --stride 4 --seed 5 --do_wandb --gradClipValue 10.0 --whiteNoiseSD 1.2 --constantOffsetSD 0.6 
+python -m brain2qwerty_v1.start_cebra --out_dir SpanishBCBL-small-cebra-default-CebUnfold_True-wNoise_1.2_0.6-GaussIn --bidir --cebra_unfolder --gru --offset 4 --batchSize 64  --random_offset --hidden 1024 --dropout 0.4 --layers 5 --nBatch 12000 --kernel 25 --stride 4 --seed 5 --do_wandb --gradClipValue 10.0 --whiteNoiseSD 1.2 --constantOffsetSD 0.6 --gauss_in
 
-python -m brain2qwerty_v1.start_cebra --out_dir SpanishBCBL-small-cebra-default-CebUnfold_True-wNoise_0.5_0.2 --bidir --cebra_unfolder --gru --offset 4 --batchSize 64  --random_offset --hidden 1024 --dropout 0.4 --layers 5 --nBatch 10000 --kernel 25 --stride 4 --seed 5 --do_wandb --gradClipValue 10.0 --whiteNoiseSD 0.5 --constantOffsetSD 0.2 
+python -m brain2qwerty_v1.start_cebra --out_dir SpanishBCBL-small-cebra-default-CebUnfold_True-wNoise_0.5_0.0 --bidir --cebra_unfolder --gru --offset 4 --batchSize 64  --random_offset --hidden 1024 --dropout 0.4 --layers 5 --nBatch 12000 --kernel 25 --stride 4 --seed 5 --do_wandb --gradClipValue 10.0 --whiteNoiseSD 0.5 --constantOffsetSD 0.0
 
-python -m brain2qwerty_v1.start_cebra --out_dir SpanishBCBL-small-cebra-default-CebUnfold_True-wNoise_0.5_0.0 --bidir --cebra_unfolder --gru --offset 4 --batchSize 64  --random_offset --hidden 1024 --dropout 0.4 --layers 5 --nBatch 10000 --kernel 25 --stride 4 --seed 5 --do_wandb --gradClipValue 10.0 --whiteNoiseSD 0.5 --constantOffsetSD 0.0
+python -m brain2qwerty_v1.start_cebra --out_dir SpanishBCBL-small-cebra-default-CebUnfold_True-wNoise_0.5_0.2 --bidir --cebra_unfolder --gru --offset 4 --batchSize 64  --random_offset --hidden 1024 --dropout 0.4 --layers 5 --nBatch 12000 --kernel 25 --stride 4 --seed 5 --do_wandb --gradClipValue 10.0 --whiteNoiseSD 0.5 --constantOffsetSD 0.2 
+
+python -m brain2qwerty_v1.start_cebra --out_dir SpanishBCBL-small-cebra-default-CebUnfold_True-wNoise_0.5_0.2-GaussIn --bidir --cebra_unfolder --gru --offset 4 --batchSize 64  --random_offset --hidden 1024 --dropout 0.4 --layers 5 --nBatch 12000 --kernel 25 --stride 4 --seed 5 --do_wandb --gradClipValue 10.0 --whiteNoiseSD 0.5 --constantOffsetSD 0.2 --gauss_in
+
+python -m brain2qwerty_v1.start_cebra --out_dir SpanishBCBL-small-cebra-default-CebUnfold_True-wNoise_0.1_0.05-GaussIn --bidir --cebra_unfolder --gru --offset 4 --batchSize 64  --random_offset --hidden 1024 --dropout 0.4 --layers 5 --nBatch 12000 --kernel 25 --stride 4 --seed 5 --do_wandb --gradClipValue 10.0 --whiteNoiseSD 0.1 --constantOffsetSD 0.05 --gauss_in
+
+python -m brain2qwerty_v1.start_cebra --out_dir SpanishBCBL-small-cebra-default-CebUnfold_True-wNoise_0.1_0.05 --bidir --cebra_unfolder --gru --offset 4 --batchSize 64  --random_offset --hidden 1024 --dropout 0.4 --layers 5 --nBatch 12000 --kernel 25 --stride 4 --seed 5 --do_wandb --gradClipValue 10.0 --whiteNoiseSD 0.1 --constantOffsetSD 0.05
+
